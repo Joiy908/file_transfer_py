06d2088e6ff5bf74153286f439f7ec549369d2f1
joiy908 <joiy9088@gmail.com>
2023-04-14 16:56:59 +0800

    add errHandler-400

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, send_from_directory, abort, Response, request
+from collections import OrderedDict
 
 app = Flask(__name__)
 
@@ -12,6 +13,13 @@ FILE_TYPE_CHECK = False
 
 messages = {'demo message'}
 
+@app.errorhandler(400)
+def bad_request(e):
+    return jsonify(OrderedDict({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })), 400
 
 @app.route('/')
 def index():
@@ -21,8 +29,8 @@ def index():
 @app.route('/path', methods=['POST'])
 def getPathTree():
     path_name = request.get_json().get('dirPath')
-    if not os.path.exists(path_name):
-        path_name = ROOT_PATH
+    if path_name is None or not os.path.exists(path_name):
+        return abort(400, "invalid dirPath")
     (currentDirName, subFolderList, subFileList) = list(os.walk(path_name))[0]
     pathTree = {'currentDirName': currentDirName,
                 'subFolderList': subFolderList,
@@ -38,7 +46,7 @@ def download():
         file_path_without_root = file_path.replace(ROOT_PATH + '/', '')
         return send_from_directory(ROOT_PATH, file_path_without_root, as_attachment=True)
     else:
-        abort(Response('File does not exists, please check the file name.'))
+        abort(400, 'File does not exists, please check the file name.')
 
 
 # upload page
@@ -49,14 +57,16 @@ def upload_file():
     else:
         if 'file' not in request.files:
             # flash('No file part')
-            return 'No file part'
+            return abort(400, "no file part")
         f = request.files.get('file')
         # get file path from URL param
         dir_path = request.args.get('dirPath')
         if not is_allowed_file(f.filename):
-            return f'upload fails, file type of {f.filename} is not permitted.'
+            return abort(400, \
+                'upload fails, file type of {f.filename} is not permitted.')
         if not os.path.exists(dir_path):
-            return f"upload fails, {dir_path} don't exist.'"
+            return abort(400, \
+                "upload fails, {dir_path} don't exist.'")
         f.save(os.path.join(dir_path, f.filename))  # 保存文件
         return 'upload successfully!'
 
@@ -66,16 +76,13 @@ def getMsgs():
     if request.method == 'POST':
         # add msg to Global
         msg = request.get_json().get('msg')
+        if msg is None:
+            return abort(400, "msg need to be provided")
         messages.add(msg)
         return "upload msg successfully."
     else:
         # if request type is GET, return messages
         return {'messages': list(messages)}
-
-
-def decode_path(file_path):
-    """rule: replace '-p*a*t*h-' with '/' """
-    return file_path.replace('-p*a*t*h-', '/')
 
 
 def is_allowed_file(filename):
